8b3bfd09e37d42261ecffb664681c1efd862b328
Guzman0406 <aguzmanlopez20@gmail.com>
2026-03-11 23:35:58 -0600

    Procedure para procesar pedidos

--- 02_procedures.sql
+++ 02_procedures.sql
@@ -20,3 +20,55 @@ EXCEPTION
 END;
 $$;
 
+
+
+-- PROCESAR PEDIDO (validar stock, crear pedido, actualizar stock, mensaje) (manejo de errores)
+CREATE OR REPLACE PROCEDURE sp_procesar_pedido (IN cliente_id INT, IN productos_json JSONB, OUT pedido_id INT)
+LANGUAGE plpgsql AS $$
+DECLARE
+    total_acumulado DECIMAL(10, 2) := 0;
+    datos_producto RECORD;
+    precio_producto DECIMAL(10, 2);
+    stock_actual INT;
+BEGIN
+
+-- Crear pedido    
+INSERT INTO pedidos (cliente_id, total) VALUES (cliente_id, 0) RETURNING id INTO pedido_id;
+
+-- Iterar sobre el JSON
+-- jsonb_to_recordset convierte el JSON en filas
+-- AS x() (x es el nombre de la tabla temporal)
+-- (producto_id INT, cantidad INT) define los tipos de datos
+FOR datos_producto IN SELECT * FROM jsonb_to_recordset(productos_json) AS x(producto_id INT, cantidad INT)
+LOOP
+-- Obtener el precio y stock
+SELECT precio, stock INTO precio_producto, stock_actual FROM productos 
+WHERE id = datos_producto.producto_id;
+
+-- Validamos si hay stock
+IF stock_actual < datos_producto.cantidad THEN
+RAISE EXCEPTION 'No hay stock para el producto con id: %', datos_producto.producto_id;
+END IF;
+
+-- Insertar los detalles
+INSERT INTO pedido_items (pedido_id, producto_id, cantidad, precio)
+VALUES (pedido_id, datos_producto.producto_id, datos_producto.cantidad, precio_producto);
+
+-- Actualizar el stock 
+UPDATE productos SET stock = stock - datos_producto.cantidad
+WHERE id = datos_producto.producto_id;
+
+-- Sumar al total
+total_acumulado := total_acumulado + (precio_producto * datos_producto.cantidad);
+END LOOP;
+
+-- Actualizar el total real
+UPDATE pedidos SET total = total_acumulado WHERE id = pedido_id;
+RAISE NOTICE 'Pedido número: % procesado con éxito y total: %', pedido_id, total_acumulado;
+
+EXCEPTION
+WHEN OTHERS THEN 
+RAISE NOTICE 'ERROR: %', SQLERRM;
+pedido_id := NULL;
+
+END; $$;
